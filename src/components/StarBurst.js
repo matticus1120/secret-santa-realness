ee660a998ebff5d31b33ae159fdb65e01a47e834
Matt McCullough <mattmccullough@Matts-MacBook-Pro.local>
2019-12-18 09:37:25 -0500

    Starburst wrapper

--- src/components/StarBurst.js
+++ src/components/StarBurst.js
@@ -3,17 +3,19 @@ import React from "react";
 export const StarBurst = () => {
     
   return (
-    <div className="starburst-wrapper">
-      <div className="starburst">
-      <a href="https://blackjet.ca/santa/?utm_source=website&utm_medium=cta&utm_campaign=secret-santa" target="_blank">
-      <div className="starburst-body">
-        <span>Available now</span>
-        <span className="starburst-main">Watch the<br/>making-of!</span>
+    <div className="starburst-outer">
+      <div className="starburst-wrapper">
+        <div className="starburst">
+        <a href="https://blackjet.ca/santa/?utm_source=website&utm_medium=cta&utm_campaign=secret-santa" target="_blank">
+        <div className="starburst-body">
+          <span>Available now</span>
+          <span className="starburst-main">Watch the<br/>making-of!</span>
+        </div>
+        </a>
       </div>
-      </a>
-    </div>
 
-  </div>
+    </div>
+    </div>
   );
 };
 
